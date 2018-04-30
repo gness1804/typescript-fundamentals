cbe12262a49ac379dd6e4d28da20bc122b43ee31
gness1804 <graham.nessler@gmail.com>
2018-04-30 16:50:34 -0500

    Add more to overloads

--- scratch-files/generics/overloads.ts
+++ scratch-files/generics/overloads.ts
@@ -1,13 +1,13 @@
 function add(x: number, y: number): number;
 function add(x: string, y: string): number;
 
-const add = (x: number|string, y: number|string): number => {
+const add = (x: number|string, y: number|string): number|undefined => {
   if (typeof x === 'string' && typeof y === 'string') {
     return parseInt(x, 10) + parseInt(y, 10);
   } else if (typeof x === 'number' && typeof y === 'number') {
     return x + y;
   }
-  return 0;
+  return undefined;
 }
 
 add('3', '15'); //works
