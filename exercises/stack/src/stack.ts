8cd70f050a4e107f416f3b28187f6a10eec2f925
gness1804 <graham.nessler@gmail.com>
2018-05-07 19:43:39 -0500

    Got first few tests passing

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -1,11 +1,24 @@
 interface IStack<T> {
   push(item: T): IStack<T>;
-  push(items: T[]): IStack<T>;
-  pop(): T | undefined;
+  // push(items: T[]): IStack<T>;
+  // pop(): T | undefined;
   length(): number;
-  print(): void;
+  // print(): void;
 }
 
-// export class Stack<T> {
+export class Stack<T> implements IStack<T> {
+  items: Array<T>;
 
-// }
+  constructor() {
+    this.items = [];
+  }
+
+  length = () => {
+    return this.items.length;
+  }
+
+  push = (item: T) => {
+    this.items.push(item);
+    return this;
+  }
+}
