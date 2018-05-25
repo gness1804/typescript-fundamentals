372866b90975254a87a31c387d5a8aff8c244c36
gness1804 <graham.nessler@gmail.com>
2018-05-24 21:08:55 -0500

    Got non-TS solution working

--- exercises/autocomplete-sfc/src/place-search-result.tsx
+++ exercises/autocomplete-sfc/src/place-search-result.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react';
-export const PlaceSearchResult: React.SFC<any> = () => {
+import { PlaceDetails } from './utils/places';
+
+// interface PropsI {
+//   rating: number,
+//   icon: string,
+//   name: string,
+//   url: string,
+//   website?: string,
+// };
+
+export const PlaceSearchResult: React.SFC<any> = ({ id, rating, icon, name, url, vicinity, }) => {
   return (
-    <div></div>
+    <div>
+      <p>{id}</p>
+      <img src={icon} alt="Restaurant icon."/>
+      <h4>{name}</h4>
+      <a href={url}>Check It Out!</a>
+      <p>Rating: {rating.toString()}</p>
+    </div>
   );
 };
