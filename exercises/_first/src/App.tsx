3cc3accbca0d95d501d312ce224a514bc204d7c2
gness1804 <graham.nessler@gmail.com>
2018-05-24 19:39:13 -0500

    Set up App

---
+++ exercises/_first/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const App: React.SFC = () => {
+  return (
+    <div>I am the App component.</div>
+  );
+}
+
+export default App;
--- exercises/_first/src/index.tsx
+++ exercises/_first/src/index.tsx
@@ -1,1 +1,14 @@
-export default () => console.log('Hello world!');
+import * as React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+const root = document.getElementById('root');
+render(<App />, root);
+
+if (module && module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => render(<App />, root));
+}
+
+export default {};
+
