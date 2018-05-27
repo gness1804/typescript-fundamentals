5f4567688d4def44d2e2751c7d076fb207aa7b2b
gness1804 <graham.nessler@gmail.com>
2018-05-27 15:40:40 -0500

    Found weaker TS solution to props issue

--- exercises/autocomplete-sfc/src/place-search-result.tsx
+++ exercises/autocomplete-sfc/src/place-search-result.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { PlaceDetails } from '../../autocomplete-3/src/utils/places';
-export const PlaceSearchResult: React.SFC<any> = (pd) => {
+
+export const PlaceSearchResult: React.SFC<any> = (pd: PlaceDetails) => {
   const { name, icon, url, vicinity, website } = pd;
 
   const websiteElem = (<a
