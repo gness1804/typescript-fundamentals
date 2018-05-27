4a8fa71581e0e2ba7d8be31baeb63f5f84d07fb0
gness1804 <graham.nessler@gmail.com>
2018-05-27 15:36:00 -0500

    Got SFC working with dynamic info, but not with TS interface

--- exercises/autocomplete-sfc/src/app.tsx
+++ exercises/autocomplete-sfc/src/app.tsx
@@ -7,24 +7,21 @@ interface IAppState {
 }
 
 export class App extends React.Component<{}, IAppState> {
-  constructor() {
-    super();
+  constructor(props: Object) {
+    super(props);
     this.state = {
       results: []
     };
   }
   async componentDidMount() {
-    let placeSummaries: PlaceSummary[] = await fetchPlaceSummaries('donut');
+    let placeSummaries: PlaceSummary[] = await fetchPlaceSummaries('pizza');
     let results: PlaceDetails[] = await fetchPlaceDetails(placeSummaries.map(p => p.place_id));
     this.setState({ results });
   }
   render() {
-    console.log(this.state.results);
-    let placeResults = this.state.results.map(pr => {
-      /////////////////////////////////////////////
-      // 👇 Replace this with your new <PlaceSearchResult /> component //
-      return <p key={pr.id}>{pr.name}</p>;
-      // return <PlaceSearchResult {...pr}/>;
+    console.log('results in render:', this.state.results);
+    const placeResults = this.state.results.map(pr => {
+      return <PlaceSearchResult {...pr}/>;
     });
     return (
       <ul className='results'>
